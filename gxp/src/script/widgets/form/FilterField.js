52248901661f1a3820843d4c49edfba344ddf7b1
Kirill Jakovlev <special-k@li.ru>
2012-06-07 16:45:10 +0400

    showed field changed (alias)

---
+++ gxp/src/script/widgets/form/FilterField.js
@@ -0,0 +1,82 @@
+(function() {
+  Ext.apply(gxp.form.FilterField.prototype, {
+
+
+    //OVERRIDED showed field changed (alias)
+    initComponent: function() {
+                
+        if(!this.filter) {
+            this.filter = this.createDefaultFilter();
+        }
+        // Maintain compatibility with QueryPanel, which relies on "remote"
+        // mode and the filterBy filter applied in it's attributeStore's load
+        // listener *after* the initial combo filtering.
+        //TODO Assume that the AttributeStore is already loaded and always
+        // create a new one without geometry fields.
+        var mode = "remote", attributes = new GeoExt.data.AttributeStore();
+        if (this.attributes) {
+            if (this.attributes.getCount() !== 0) {
+                mode = "local";
+                this.attributes.each(function(r) {
+                    var match = /gml:((Multi)?(Point|Line|Polygon|Curve|Surface|Geometry)).*/.exec(r.get("type"));
+                    if( !match ) attributes.add([r]);
+                    //match || attributes.add([r]);
+                });
+            } else {
+                attributes = this.attributes;
+            }
+        }
+
+        var defAttributesComboConfig = {
+            xtype: "combo",
+            store: attributes,
+            editable: mode == "local",
+            typeAhead: true,
+            forceSelection: true,
+            mode: mode,
+            triggerAction: "all",
+            allowBlank: this.allowBlank,
+            displayField: "alias",
+            valueField: "name",
+            value: this.filter.property,
+            listeners: {
+                select: function(combo, record) {
+                    this.items.get(1).enable();
+                    this.filter.property = record.get("name");
+                    this.fireEvent("change", this.filter);
+                },
+                // workaround for select event not being fired when tab is hit
+                // after field was autocompleted with forceSelection
+                "blur": function(combo) {
+                    var index = combo.store.findExact("name", combo.getValue());
+                    if (index != -1) {
+                        combo.fireEvent("select", combo, combo.store.getAt(index));
+                    } else if (combo.startValue !== null) {
+                        combo.setValue(combo.startValue);
+                    }
+                },
+                scope: this
+            },
+            width: 120
+        };
+        this.attributesComboConfig = this.attributesComboConfig || {};
+        Ext.applyIf(this.attributesComboConfig, defAttributesComboConfig);
+
+        this.items = this.createFilterItems();
+        
+        this.addEvents(
+            /**
+             * Event: change
+             * Fires when the filter changes.
+             *
+             * Listener arguments:
+             * filter - {OpenLayers.Filter} This filter.
+             */
+            "change"
+        ); 
+
+        gxp.form.FilterField.superclass.initComponent.call(this);
+    }
+
+  });
+})();
